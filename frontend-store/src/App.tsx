eb0153f3eb137cf468666dfe68d017d37512ca7e
naromano <naromano95@gmail.com>
2026-06-13 07:28:04 -0300

    se arreglaron ls rutas de foodstore

--- frontend-store/src/App.tsx
+++ frontend-store/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useAuthStore } from './store/useAuthStore'
 import Layout from './components/Layout'
@@ -90,6 +90,7 @@ function App() {
                 }
               />
             </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </AuthInitializer>
       </BrowserRouter>
